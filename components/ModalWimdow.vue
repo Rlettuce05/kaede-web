69e73ee42d3576d867829c8073ef339af55c8e7b
syunsuke okamura <syunrobin@gmail.com>
2022-03-29 16:45:18 +0900

    ✒ ModalWindow.vue : template part

---
+++ components/ModalWimdow.vue
@@ -0,0 +1,12 @@
+<template>
+    <div class="trans">
+        <div class="overlay">
+            <div class="window">
+                <div class="content">
+                    <slot />
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
+
