3075e18059e371f57e933c146ac6a4a7425b725f
syunsuke okamura <syunrobin@gmail.com>
2022-04-01 23:07:27 +0900

    ✒ Navbar.vue : navbar interface

--- components/Navbar.vue
+++ components/Navbar.vue
@@ -1,10 +1,21 @@
 <template>
   <div class='content'>
     <b-container>
-      <p>test</p>
+      <b-row>
+        <b-col sm="3">
+          <nuxt-link>ABOUT</nuxt-link>
+        </b-col>
+        <b-col sm="3">
+          <nuxt-link>WORKS</nuxt-link>
+        </b-col>
+        <b-col sm="3">
+          <nuxt-link>CONTACT</nuxt-link>
+        </b-col>
+      </b-row>
     </b-container>
   </div>
 </template>
+
 <script>
 export default {
   name: 'NavBar'
