b1c3a3b27bd7ca6403af3487ea24b044a15288cf
syunsuke okamura <syunrobin@gmail.com>
2022-04-02 23:51:36 +0900

    ✒ About.vue : pagetitle, hr

--- pages/About.vue
+++ pages/About.vue
@@ -1,6 +1,21 @@
 <template>
-  <NavBar />
+  <div>
+    <NavBar />
+    <div class="content">
+      <div class="container">
+        <h1 class="PageTitle">About</h1>
+        <hr class="col-5">
+      </div>
+    </div>
+  </div>
 </template>
+
+<style scoped>
+hr {
+  float: left;
+  background-color: black;
+}
+</style>
 
 <script>
 import NavBar from '~/components/Navbar.vue'
