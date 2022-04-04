87bc0c75015d11f5a6bb33ce59c6b1012d0740c8
syunsuke okamura <syunrobin@gmail.com>
2022-04-04 22:25:51 +0900

    ✒ Navbar.vue : centerize

--- components/Navbar.vue
+++ components/Navbar.vue
@@ -2,19 +2,28 @@
   <div class='content'>
     <b-container>
       <b-row>
-        <b-col sm="3">
-          <nuxt-link to="/About">ABOUT</nuxt-link>
+        <b-col cols="4">
+          <nuxt-link to="/About" class="linktext">ABOUT</nuxt-link>
         </b-col>
-        <b-col sm="3">
-          <nuxt-link to="/Works">WORKS</nuxt-link>
+        <b-col cols="4">
+          <nuxt-link to="/Works" class="linktext">WORKS</nuxt-link>
         </b-col>
-        <b-col sm="3">
-          <nuxt-link to="/Contact">CONTACT</nuxt-link>
+        <b-col cols="4">
+          <nuxt-link to="/Contact" class="linktext">CONTACT</nuxt-link>
         </b-col>
       </b-row>
     </b-container>
   </div>
 </template>
+
+<style scoped>
+.content {
+  text-align: center;
+}
+.linktext {
+  text-align: center;
+}
+</style>
 
 <script>
 export default {
