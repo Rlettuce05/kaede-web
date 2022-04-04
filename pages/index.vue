196a4435bf284b61a368ef41f25812a9dd2e6f3d
syunsuke okamura <syunrobin@gmail.com>
2022-04-04 23:55:12 +0900

    ✒ index.vue : pagelink btn

--- pages/index.vue
+++ pages/index.vue
@@ -1,9 +1,24 @@
 <template>
   <div class="content">
+    <b-container>
+      <!-- img -->
+      <b-row class="pagwlinks-row">
+        <b-col class="pagelinks-col">
+          <b-btn class="pagelinks" href="Works">WORKS</b-btn>
+        </b-col>
+        <b-col class="pagelinks-col">
+          <b-btn class="pagelinks" href="About">ABOUT</b-btn>
+        </b-col>
+        <b-col class="pagelinks-col">
+          <b-btn class="pagelinks" href="Commission">COMMISSION</b-btn>
+        </b-col>
+      </b-row>
+    </b-container>
   </div>
 </template>
 
-
+<style scoped>
+</style>
 
 <script>
 export default {
