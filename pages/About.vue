66394d243edb4e4f09739b63ee1e469776baada2
RLettuce <syunrobin@gmail.com>
2022-04-05 14:03:19 +0900

    ✒ About.vue : content

--- pages/About.vue
+++ pages/About.vue
@@ -4,6 +4,14 @@
     <div class="content">
       <h1 class="PageTitle">About</h1>
       <b-container>
+        <b-row>
+          <b-col col="8">
+            <p><!--ここに紹介文--></p>
+          </b-col>
+          <b-col col="4">
+            <b-img src="../static/icon.jpeg"></b-img>
+          </b-col>
+        </b-row>
       </b-container>
     </div>
   </div>
