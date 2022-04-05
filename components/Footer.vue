4e8d8f25ef4edeb86b20d0b8333588d2c0c65776
syunsuke okamura <syunrobin@gmail.com>
2022-04-05 12:51:17 +0900

    ✒ Footer.vue : centerize

--- components/Footer.vue
+++ components/Footer.vue
@@ -1,12 +1,14 @@
 <template>
-    <div class="content">
-        <p>©2022 紙雨楓</p>
-        <p>Design by RLettuce</p>
-    </div>
+  <div class="content">
+    <p>©2022 紙雨楓</p>
+    <p>Design by RLettuce</p>
+  </div>
 </template>
 
 <style scoped>
-
+.content {
+  text-align: center;
+}
 </style>
 
 <script>
