b9f47943701e5eafecf7114e202c18d0a2d201ae
RLettuce <syunrobin@gmail.com>
2022-04-05 13:45:53 +0900

    ✒ index.vue : link youtube, twitter

--- pages/index.vue
+++ pages/index.vue
@@ -16,6 +16,12 @@
         </b-col>
       </b-row>
       <b-row class="links-row">
+        <b-col class="links-col">
+          <b-btn class="links" href="https://www.youtube.com/channel/UCQu9wEzbJKYqBQLSz8MQKTg"><b-icon icon="youtube"></b-icon></b-btn>
+        </b-col>
+        <b-col class="links-col">
+          <b-btn class="links" href="https://twitter.com/ka3de_KA"><b-icon icon="twitter"></b-icon></b-btn>
+        </b-col>
       </b-row>
     </b-container>
   </div>
@@ -25,7 +31,13 @@
 </style>
 
 <script>
+import {BIcon, BIconYoutube, BIconTwitter} from 'bootstrap-vue'
 export default {
-  name: 'IndexPage'
+  name: 'IndexPage',
+  components: {
+    BIcon,
+    BIconYoutube,
+    BIconTwitter
+  }
 }
 </script>
