07e69cd28c282ba3128740ea8648f801dc4db21a
RLettuce <syunrobin@gmail.com>
2022-04-06 12:22:56 +0900

    ✒ Works.vue : rendering content

--- pages/Works.vue
+++ pages/Works.vue
@@ -4,8 +4,16 @@
     <div class="content">
       <h1 class="PageTitle">Works</h1>
       <b-container>
-        <div v-for="work in works" :key="work.id">
-        </div>
+        <b-row>
+          <b-col col="4" v-for="work in works" :key="work.id">
+            <a v-bind:href="work.link">
+              <img v-bind:src="work.img">
+            </a>
+            <div class="title">
+              {{ work.title }}
+            </div>
+          </b-col>
+        </b-row>
       </b-container>
     </div>
   </div>
@@ -14,6 +22,9 @@
 <style scoped>
 .PageTitle {
   text-align: center;
+}
+b-container {
+  margin: 5%;
 }
 </style>
 
