86637d70a98d0f64da5dd87e5d2e6e6adb6535eb
Chipsnet <doronomori660@outlook.jp>
2023-01-15 01:25:41 +0900

    🐛 fix video ratio

--- pages/index.vue
+++ pages/index.vue
@@ -3,7 +3,7 @@
         <div class="background"></div>
         <transition>
             <div class="content">
-                <div class="embed-responsive embed-responsive-16by9">
+                <div class="ratio ratio-16x9">
                     <iframe
                         class="embed-responsive-item"
                         src="https://www.youtube-nocookie.com/embed/MBftCfroEjI?rel=0&player.setVolume(5)"
